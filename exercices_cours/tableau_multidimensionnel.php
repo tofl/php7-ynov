de289b83e3478ffd1e6c4ec681dd2edb6ccf7ad6
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-03-19 16:31:46 +0100

    Added a new exercice

---
+++ exercices_cours/tableau_multidimensionnel.php
@@ -0,0 +1,13 @@
+<?php
+
+$films = [
+    'Total recall' => ['Arnold Schwarzenegger', 1990, 'Paul Verhoeven'],
+    'Inception' => ['Leonardo DiCaprio', 2010, 'Christopher Nolan'],
+    'Wall Street' => ['Michael Douglas', 1987, 'Oliver Stone'],
+    'Ghosts of Mars' => ['Ice Cube', 2001, 'John Carpenter'],
+    'Some like it hot' => ['Marilyn Monroe', 1959, 'Billy Wilder'],
+    'Django' => ['Leonardo DiCaprio', 2012, 'Quentin Tarantino'],
+    ['Avatar', 'Ghost in the shell', 'Vacancy', 'La Grande Vadrouille']
+];
+
+var_dump($films);
